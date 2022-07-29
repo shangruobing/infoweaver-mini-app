85bc1ac98ba62e7102473cdc69562095f5221259
Ruobing Shang <shangruobing29@163.com>
2022-07-29 10:51:01 +0800

    Feature the English pronounce page.

--- pages/formula/Index.vue
+++ pages/formula/Index.vue
@@ -8,21 +8,25 @@
     </view>
 
     <view class="button-group">
-      <button @click="play" class="next">发音</button>
-      <button @click="previous" class="next">上一个</button>
-      <button @click="change" class="next">下一个</button>
+      <view class="pronounce">
+        <button @click="Americanplay">美式发音</button>
+        <button @click="Englishplay">英式发音</button>
+      </view>
+      <button @click="previous">上一个</button>
+      <button @click="change" type="primary">下一个</button>
     </view>
   </view>
 </template>
 
 <script setup>
-import { reactive, ref, watchEffect } from 'vue'
+import { ref, reactive, watchEffect } from 'vue'
 import tableData from './tableData.js'
+
+const index = ref(0)
 const item = reactive({
   word: 'confidence',
-  translation: '自信'
+  translation: 'n.自信心'
 })
-const index = ref(0)
 
 tableData.sort(() => 0.5 - Math.random())
 
@@ -56,10 +60,23 @@ watchEffect(() => {
   item.translation = randomItem.translation
 })
 
-const play = () => {
+const Americanplay = () => {
   const innerAudioContext = uni.createInnerAudioContext()
   innerAudioContext.autoplay = true
   innerAudioContext.src = 'http://dict.youdao.com/dictvoice?type=0&audio=' + item.word
+  innerAudioContext.onPlay(() => {
+    // console.log('开始播放')
+  })
+  innerAudioContext.onError((res) => {
+    console.log(res.errMsg)
+    console.log(res.errCode)
+  })
+}
+
+const Englishplay = () => {
+  const innerAudioContext = uni.createInnerAudioContext()
+  innerAudioContext.autoplay = true
+  innerAudioContext.src = 'http://dict.youdao.com/dictvoice?type=1&audio=' + item.word
   innerAudioContext.onPlay(() => {
     // console.log('开始播放')
   })
@@ -73,19 +90,35 @@ const play = () => {
 view {
   text-align: center;
 }
+
 .word {
   font-family: 'Consolas';
-  margin-top: 30px;
+  margin-top: 50px;
   font-size: 40px;
   font-weight: bolder;
   margin-bottom: 30px;
 }
+
 .translation {
   font-size: x-large;
 }
+
 .button-group {
   position: absolute;
   width: 100%;
-  bottom: 20%;
+  bottom: 15%;
+  > button {
+    margin: 0 1em;
+    margin-bottom: 25rpx;
+  }
+}
+
+.pronounce {
+  display: flex;
+  width: 100%;
+  > button {
+    width: calc(50% - 2em);
+    margin-bottom: 25rpx;
+  }
 }
 </style>
